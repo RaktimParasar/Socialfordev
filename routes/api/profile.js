162683960262358ee92382900310b4df2ba935f9
RaktimParasar <raktimparasar96@gmail.com>
2020-11-08 13:24:49 +0530

    feat: delete profile and user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -116,4 +116,20 @@ router.get('/user/:user_id', async (req, res) => {
     }
 });
 
+//@route DELETE api/profile
+//@desc Delete profile, user, posts
+//@access private
+router.delete('/', auth, async (req, res) => {
+    try {
+        //Delete profile
+        await Profile.findOneAndRemove({ user: req.user.id });
+        //Delete user
+        await User.findOneAndRemove({ _id: req.user.id });
+        res.json({ msg: 'User Deleted' });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Serevr Error');
+    }
+});
+
 module.exports = router;
