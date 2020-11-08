f5fc312dad92bb4833b21d154869933724c872a1
RaktimParasar <raktimparasar96@gmail.com>
2020-11-08 21:25:25 +0530

    feat: Add and Delete experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -132,4 +132,56 @@ router.delete('/', auth, async (req, res) => {
     }
 });
 
+//@route PUT api/profile/experience
+//@desc Add profile experience
+//@access private
+router.put('/experience', [auth, [
+    check('title', 'Title is required').not().isEmpty(),
+    check('company', 'Company is required').not().isEmpty(),
+    check('from', 'From date is required').not().isEmpty()
+    
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const { title, company, location, from, to, current, description } = req.body;
+    
+    const newExp = {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    }
+    try {
+        //find user and add experience with most recent at top
+        const profile = await Profile.findOne({ user: req.user.id });
+        profile.experience.unshift(newExp);
+        await profile.save();
+        res.json(profile)
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+//@route DELETE api/profile/experience/:exp_id
+//@desc Delete experience from profile
+//@access private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+        
+        //Get remove index by matching id
+        const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+        profile.experience.splice(removeIndex, 1);
+        await profile.save();
+        res.json(profile);
+    } catch (error) {
+        console.error()
+    }
+})
 module.exports = router;
