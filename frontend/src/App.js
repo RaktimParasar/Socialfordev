ef622722fb6ec714a4a1112f19a45a315c30217d
RaktimParasar <raktimparasar96@gmail.com>
2020-11-15 12:23:38 +0530

    feat: profile component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 //redux
 import store from './store';
@@ -36,6 +37,7 @@ const App = () => {
             <Route exact path='/login' component={Login} />
             <Route exact path='/register' component={Register} />
             <PrivateRoute exact path='/dashboard' component={Dashboard} />
+            <PrivateRoute exact path='/create-profile' component={CreateProfile} />
           </Switch>
     </Fragment>
     </Router>
