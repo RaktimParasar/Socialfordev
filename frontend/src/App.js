0d8ff6f091c9c0ed4d2a560607186ba794efe06e
RaktimParasar <raktimparasar96@gmail.com>
2020-11-19 19:33:14 +0530

    feat: Display profiles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,9 @@ import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
+import Profiles from './components/profiles/Profiles';
 import PrivateRoute from './components/routing/PrivateRoute';
+
 //redux
 import store from './store';
 import { Provider } from 'react-redux';
@@ -39,6 +41,7 @@ const App = () => {
           <Switch>
             <Route exact path='/login' component={Login} />
             <Route exact path='/register' component={Register} />
+            <Route exact path='/profiles' component={Profiles} />
             <PrivateRoute exact path='/dashboard' component={Dashboard} />
             <PrivateRoute exact path='/create-profile' component={CreateProfile} />
             <PrivateRoute exact path='/edit-profile' component={EditProfile} />
